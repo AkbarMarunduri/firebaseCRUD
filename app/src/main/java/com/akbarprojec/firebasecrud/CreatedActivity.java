06a477f0c8f87abee38da63dd2355aa46a2894d4
AkbarMarunduri <55292439+AkbarMarunduri@users.noreply.github.com>
2020-08-09 13:52:13 +0700

    belajar menggunakan firebase CRUD

---
+++ app/src/main/java/com/akbarprojec/firebasecrud/CreatedActivity.java
@@ -0,0 +1,80 @@
+package com.akbarprojec.firebasecrud;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.akbarprojec.firebasecrud.Model.Mahasiswa;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreatedActivity extends AppCompatActivity implements View.OnClickListener {
+private EditText edNim, edNama;
+    private Button btSave;
+    private Mahasiswa mahasiswa;
+
+    //menghubungkan ke firebase dengan ini
+    DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_created);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        edNim = findViewById(R.id.edt_nim);
+        edNama = findViewById(R.id.edt_nama);
+        btSave = findViewById(R.id.btn_save);
+
+        btSave.setOnClickListener(this);
+
+        mahasiswa = new Mahasiswa();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.btn_save) {
+            SaveMahasiswa();
+        }
+    }
+
+    void SaveMahasiswa() {
+        String nama = edNama.getText().toString().trim();
+        String nim = edNim.getText().toString().trim();
+
+        boolean isEmptyField = false;
+
+        if (TextUtils.isEmpty(nama)) {
+            isEmptyField = true;
+            edNama.setError("Field ini tidakboleh kosong");
+        }
+        if (TextUtils.isEmpty(nim)) {
+            isEmptyField = true;
+            edNim.setError("Field ini tidak boleh kosong");
+        }
+
+        if (!isEmptyField) {
+            Toast.makeText(CreatedActivity.this, "Save Data....!!", Toast.LENGTH_SHORT).show();
+            DatabaseReference dbMahasiswa = mDatabase.child("guru");
+
+            String id = dbMahasiswa.push().getKey();
+            mahasiswa.setId(id);
+            mahasiswa.setNama(nama);
+            mahasiswa.setNim(nim);
+            mahasiswa.setFoto("");
+
+            //insert data
+            dbMahasiswa.child(id).setValue(mahasiswa);
+            finish();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/akbarprojec/firebasecrud/Model/Mahasiswa.java
@@ -0,0 +1,76 @@
+package com.akbarprojec.firebasecrud.Model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Mahasiswa implements Parcelable {
+    private String id, nim, nama, foto;
+
+    public Mahasiswa() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel desc, int flag) {
+        desc.writeString(this.id);
+        desc.writeString(this.nama);
+        desc.writeString(this.nim);
+        desc.writeString(this.foto);
+    }
+
+    protected Mahasiswa(Parcel in) {
+        this.id = in.readString();
+        this.nim = in.readString();
+        this.nama = in.readString();
+        this.foto = in.readString();
+    }
+
+    public static final Parcelable.Creator<Mahasiswa> CREATOR = new Creator<Mahasiswa>() {
+        @Override
+        public Mahasiswa createFromParcel(Parcel parcel) {
+            return new Mahasiswa(parcel);
+        }
+
+        @Override
+        public Mahasiswa[] newArray(int i) {
+            return new Mahasiswa[i];
+        }
+    };
+}
